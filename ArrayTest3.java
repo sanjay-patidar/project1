853e4e08556aa0d38f0c95124a9a9696cd07e71a
sanjay-patidar <sanjaypatidar3@gmail.com>
2017-12-23 12:39:47 +0530

    new file

---
+++ ArrayTest3.java
@@ -0,0 +1,14 @@
+class ArrayTest3
+{
+	public static void main(String[] args)
+	{
+	int[] x=new int[6];
+	//System.out.println(x.length());
+	System.out.println(x.length);
+
+	String s="sanjay";
+	//System.out.println(s.length);
+	System.out.println(s.length());
+
+	}
+}
